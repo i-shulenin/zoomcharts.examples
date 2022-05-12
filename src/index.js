d996447a640c7a8c1c1d244f94987e3a4d7312ee
Ilya Shulenin <ilya@shulenin.com>
2022-05-12 17:04:27 +0300

    Add ZoomCharts

--- src/index.js
+++ src/index.js
@@ -1,10 +1,78 @@
-import "./styles.css";
+import ZoomCharts from '@dvsl/zoomcharts';
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+document.addEventListener('DOMContentLoaded', async () => {
+  const exportData = document.getElementById('export');
+
+  const data = {
+        "nodes": [
+            {
+                          "id": "n1",
+                          "loaded": true,
+                          "style": {
+                                            "label": "Node1"
+                                        },
+                          "resetCoordinates": false,
+                          "resetLocked": false,
+                          "x": 5.677291180632975,
+                          "y": 157.89821055364224
+                      },
+            {
+                          "id": "n2",
+                          "loaded": true,
+                          "style": {
+                                            "label": "Node2"
+                                        },
+                          "resetCoordinates": false,
+                          "resetLocked": false,
+                          "x": 62.88490720742704,
+                          "y": -36.148386429172454
+                      },
+            {
+                          "id": "n3",
+                          "loaded": true,
+                          "style": {
+                                            "label": "Node3"
+                                        },
+                          "resetCoordinates": false,
+                          "resetLocked": false,
+                          "x": -49.18120248475649,
+                          "y": -39.10136945835826
+                      }
+        ],
+        "links": [
+            {
+                          "id": "l1",
+                          "from": "n1",
+                          "to": "n2"
+                      },
+            {
+                          "id": "l2",
+                          "from": "n2",
+                          "to": "n3"
+                      },
+                  {
+                                "id": "l3",
+                                "from": "n3",
+                                "to": "n1"
+                            }
+              ]
+  };
+
+  const netChart = new ZoomCharts.NetChart({
+    container: 'netchart',
+    data: {
+      preloaded: data
+    },
+    layout: {
+    mode: 'static',
+    }
+  });
+
+  exportData.addEventListener('click', () => {
+    const dataset = netChart.exportData({
+      exportCoordinates: true,
+    })
+
+    console.log(dataset);
+  });
+});
